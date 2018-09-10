31243e15fad90edda379026b66bdad6216fa6cd7
Reiichiro Nakano <reiichiro.s.nakano@gmail.com>
2018-09-11 00:41:46 +0900

    lint

--- js/main.js
+++ js/main.js
@@ -16,30 +16,30 @@ class Main {
    */
   constructor() {
     // Initiate variables
-    this.chatTextBox = document.getElementById("generated-chat");
-    this.inputText = document.getElementById("input-text");
-    this.chatButton = document.getElementById("chat-button");
+    this.chatTextBox = document.getElementById('generated-chat');
+    this.inputText = document.getElementById('input-text');
+    this.chatButton = document.getElementById('chat-button');
     this.chatButton.onclick = () => {
       this.sendChat();
-    }
+    };
     this.chatContent = [];
-    
+
     Promise.all([
         tf.loadModel('decoder-model/model.json'),
-        tf.loadModel('encoder-model/model.json')
+        tf.loadModel('encoder-model/model.json'),
     ]).then(([decoder, encoder]) => {
         this.decoder = decoder;
         this.encoder = encoder;
         this.enableGeneration();
-    })
+    });
   }
 
   /**
-   * Called after model has finished loading or generating. 
+   * Called after model has finished loading or generating.
    * Sets up UI elements for generating text.
    */
   enableGeneration() {
-    this.chatButton.innerText = "Send";
+    this.chatButton.innerText = 'Send';
     this.chatButton.disabled = false;
   }
 
@@ -52,7 +52,7 @@ class Main {
     const states = tf.tidy(() => {
         const input = this.convertSentenceToTensor(inputText);
         return this.encoder.predict(input);
-    })
+    });
 
     this.decoder.layers[1].resetStates(states);
 
@@ -69,7 +69,7 @@ class Main {
 
         const outputToken = await outputTokenTensor.data();
         outputTokenTensor.dispose();
-        const word = targetIdx2word[outputToken[0]]
+        const word = targetIdx2word[outputToken[0]];
         numPredicted++;
         nextTokenID = outputToken[0];
         console.log(outputToken, word);
@@ -78,7 +78,8 @@ class Main {
             responseTokens.push(word);
         }
 
-        if (word === '<EOS>' || numPredicted >= wordContext.decoder_max_seq_length) {
+        if (word === '<EOS>'
+            || numPredicted >= wordContext.decoder_max_seq_length) {
             terminate = true;
         }
 
@@ -99,26 +100,28 @@ class Main {
   }
 
   convertSentenceToTensor(sentence) {
-    let input_wids = [];
-    wordTokenize(sentence).map(x => {
+    let inputWordIds = [];
+    wordTokenize(sentence).map((x) => {
         x = x.toLowerCase();
-        let idx = "1";
+        let idx = '1';
         if (x in inputWord2idx) {
             idx = inputWord2idx[x];
         }
-        input_wids.push(Number(idx));
-    })
-    if (input_wids.length < wordContext.encoder_max_seq_length) {
-        input_wids = 
+        inputWordIds.push(Number(idx));
+    });
+    if (inputWordIds.length < wordContext.encoder_max_seq_length) {
+        inputWordIds =
             Array.concat(
-                new Array(wordContext.encoder_max_seq_length-input_wids.length+1).join('0').split('').map(Number), 
-                input_wids
+                new Array(
+                    wordContext.encoder_max_seq_length-inputWordIds.length+1)
+                    .join('0').split('').map(Number),
+                inputWordIds
             );
     } else {
-        input_wids = input_wids.slice(0, 18);
+        inputWordIds = inputWordIds.slice(0, 18);
     }
-    console.log(input_wids);
-    return tf.tensor2d(input_wids, [1, 18]);
+    console.log(inputWordIds);
+    return tf.tensor2d(inputWordIds, [1, 18]);
   }
 
   convertTokensToSentence(tokens) {
@@ -130,13 +133,13 @@ class Main {
 
       let textBoxString = '';
       for (let i = 0; i < this.chatContent.length; i++) {
-          textBoxString += this.chatContent[i].user + ': ' + this.chatContent[i].text + '\n\n';
+          textBoxString +=
+            this.chatContent[i].user + ': ' + this.chatContent[i].text + '\n\n';
       }
 
       this.chatTextBox.innerText = textBoxString;
       console.log(this.chatContent);
   }
-
 }
 
 window.addEventListener('load', () => new Main());
